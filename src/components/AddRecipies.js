0b9355f4b535ee4252aeb101094f630ec1b0a8a1
sruthybabu <sruthybabu64@gmail.com>
2022-05-30 01:51:12 +0530

    navigation bar complete

--- src/components/AddRecipies.js
+++ src/components/AddRecipies.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const AddRecipies = () => {
     var [recipename,setRecipename]=useState("")
@@ -25,6 +26,7 @@ const AddRecipies = () => {
     }
   return (
     <div>
+        <Navbar/>
         <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
